79788abc3b15f26aa4b3e99ba3795e1b5dcaf00e
Artemsav <studentbedn@gmail.com>
2018-03-16 22:36:51 +0300

    fix

--- pprint_json.py
+++ pprint_json.py
@@ -1,13 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
 import json
+import argparse
+import sys
 
+def load_data():
+    filepath= argparse.ArgumentParser()
+    filepath.add_argument('path', type=argparse.FileType(mode='r', bufsize=-1,encoding="utf-8",errors=None), help='Type in a path!')
+    """with open(filepath, 'r') as filepath:
+        filepath.read()"""
+    return filepath
 
-def load_data(filepath):
-    pass
+def pretty_print_json():
 
+    namespace = load_data().parse_args(sys.argv[1:])
+    text = namespace.path.read()
+    return text
 
-def pretty_print_json(data):
-    pass
 
 
 if __name__ == '__main__':
-    pass
+    #filepath=load_data()
+    #namespace=filepath.parse_args(sys.argv[1:])
+    #text=namespace.path.read()
+    print (json.dumps({pretty_print_json()}, sort_keys=True, indent=6))
+    #print (json.dumps({pretty_print_json()}))
+    #print (json.loads(pretty_print_json()))
