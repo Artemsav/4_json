a90936a3de670f8add84906d1047e8b6bb566f1b
Artemsav <studentbedn@gmail.com>
2018-03-24 00:05:08 +0300

    Fix names troubles, fix parsing function(make it)

--- pprint_json.py
+++ pprint_json.py
@@ -3,29 +3,30 @@ import argparse
 
 
 def load_data():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('path')
-    filepath = parser.parse_args()
-    with open(filepath.path) as f:
-        text = f.read()
+    with open(parser_data().path) as file:
+        text = file.read()
         pretty_loads = json.loads(text)
     return pretty_loads
 
 
-def pretty_print_json():
+def pretty_json():
     pretty_print = (json.dumps(load_data(), sort_keys=True, indent=6))
     return pretty_print
 
 
 if __name__ == '__main__':
+
+    def parser_data():
+        parser = argparse.ArgumentParser()
+        parser.add_argument('path')
+        filepath = parser.parse_args()
+        return filepath
+
     try:
-        print (pretty_print_json())
+        print (pretty_json())
     except IndexError:
-        print ('Type in a path to the .json file')
-        exit()
+        exit('Type in a path to the .json file')
     except FileNotFoundError:
-        print ('No such file in directory')
-        exit()
+        exit('No such file in directory')
     except ValueError:
-        print ('Error. File have to be in .json format')
-        exit()
+        exit('Error. File have to be in .json format')
