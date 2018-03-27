f54265b37df3a3a1a6d02ed461a7c9245ab90bc4
Artemsav <studentbedn@gmail.com>
2018-03-27 23:06:09 +0300

    remake my programm, add arguments in functions

--- pprint_json.py
+++ pprint_json.py
@@ -2,31 +2,28 @@ import json
 import argparse
 
 
-def load_data():
-    with open(parser_data().path) as file:
-        text = file.read()
-        pretty_loads = json.loads(text)
-    return pretty_loads
-
-
-def pretty_json():
-    pretty_print = (json.dumps(load_data(), sort_keys=True, indent=6))
-    return pretty_print
-
-
-if __name__ == '__main__':
-
-    def parser_data():
-        parser = argparse.ArgumentParser()
-        parser.add_argument('path')
-        filepath = parser.parse_args()
-        return filepath
-
+def load_data(myfile):
     try:
-        print (pretty_json())
+        with open(myfile) as file:
+            text = file.read()
+        return json.loads(text)
     except IndexError:
         exit('Type in a path to the .json file')
     except FileNotFoundError:
         exit('No such file in directory')
     except ValueError:
         exit('Error. File have to be in .json format')
+
+
+def pretty_json(json_data):
+    pretty_print = (json.dumps(load_data(myfile), sort_keys=True, indent=6))
+    return pretty_print
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('path')
+    filepath = parser.parse_args()
+    myfile = filepath.path
+    json_data = load_data(myfile)
+    print(pretty_json(json_data))
